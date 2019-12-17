6eed6ad855dd01ff5eb1bc7edae39845d32548ec
baker <baker.he@zoom.us>
2019-12-17 12:15:18 +0800

    支持祝福文字显示图片

--- custom.js
+++ custom.js
@@ -4,6 +4,12 @@ $(function () {
     config.texts.forEach(function (item) {
         let p = document.createElement("p");
         p.innerHTML = item;
+        if (config.imgs[item]) {
+            let img = document.createElement("img");
+            img.src = config.imgs[item];
+            img.setAttribute("class", 'text-img');
+            p.appendChild(img);
+        }
         dom.appendChild(p);
     });
     $("#texts-container").append(dom.innerHTML);
